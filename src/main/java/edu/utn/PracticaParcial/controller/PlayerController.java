59dde841d89fcd313f97962b9c7d3e0a261cbd00
Mauro Guerreiro <maurogch@gmail.com>
2019-06-03 22:57:13 -0300

    header

--- src/main/java/edu/utn/PracticaParcial/controller/PlayerController.java
+++ src/main/java/edu/utn/PracticaParcial/controller/PlayerController.java
@@ -6,13 +6,16 @@ import edu.utn.PracticaParcial.repository.PlayerRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,6 +43,11 @@ public class PlayerController {
     public void add(@RequestBody @Valid Player player){
         playerRepository.save(player);
     }*/
+
+    @GetMapping("/header")
+    public String getUserAgent(@RequestHeader("User-Agent") String userAgent) {
+        return userAgent;
+    }
 
     @PostMapping("")
     @ResponseStatus(HttpStatus.CREATED)
